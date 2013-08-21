261728c844e73b4530f3dba831138b08f2869327
Konrad Pason <konrad@macbook-pro.home>
2013-08-21 13:48:09 +0200

    Saving data in the controller

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -2,7 +2,15 @@ class PostsController < ApplicationController
   def new
   end
 
-  def create
-  render text: params[:post].inspect
+ def create
+  @post = Post.new(post_params)
+
+  @post.save
+  redirect_to @post
 end
+
+private
+  def post_params
+    params.require(:post).permit(:title, :text)
   end
+end
