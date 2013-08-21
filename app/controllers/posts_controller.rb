9c27f95eeba8de9354e816962be999b2e97cf9f6
Jakub Gorzelak <jakub.gorzelak@gmail.com>
2013-08-21 15:35:11 +0200

    Use proper indent

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,23 +1,27 @@
 class PostsController < ApplicationController
+
   def new
   end
 
-   def create
+  def create
     @post = Post.new(params[:post].permit(:title, :text))
 
     @post.save
     redirect_to @post
   end
 
-private
-  def post_params
-    params.require(:post).permit(:title, :text)
+  def show
+    @post = Post.find(params[:id])
   end
-end
-def show
-  @post = Post.find(params[:id])
-end
-end
-def index
-  @posts = Post.all
+
+  def index
+    @posts = Post.all
+  end
+
+  private
+
+    def post_params
+      params.require(:post).permit(:title, :text)
+    end
+
 end
